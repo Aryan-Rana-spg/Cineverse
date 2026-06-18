b29f52877057eefeae21f8714a5a5f4c28122312
Aryan-Rana-spg <aryanaf203@gmail.com>
2026-06-18 13:49:25 +0530

    chnages to the project

--- backend/auth-service/src/main/java/com/cineverse/auth/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/cineverse/auth/AuthServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.cineverse.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+@ComponentScan(basePackages = "com.cineverse") // Tells Spring to scan all your sub-packages
+public class AuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 }
